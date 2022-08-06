ec035bdea59cbedcd57e5a315c3ee6ba2d36aee9
Sam McCord <sam.mccord@protonmail.com>
2022-08-06 12:38:49 -0600

    refamiliarize myself with solidity

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -2,13 +2,40 @@
 
 pragma solidity ^0.8.7;
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// NOTE: This is just a test contract, please delete me
+contract Counter is Ownable, ERC20, PaymentSplitter {
+    using SafeMath for uint;
+    using SafeERC20 for address;
 
-contract Counter {
     uint256 count = 0;
+    mapping(address => uint256) private _received;
 
     event CountedTo(uint256 number);
+
+    constructor(string memory name, string memory symbol,  address[] memory payees, uint256[] memory shares) Ownable() ERC20(name, symbol) PaymentSplitter(payees, shares)  {}
+
+    receive() external payable virtual override {
+        _received[msg.sender] += msg.value;
+        emit PaymentReceived(_msgSender(), msg.value);
+    }
+
+    function release(address payable account) public override {
+        require(_received[account] > 0, "Counter: account has not deposited");
+        super.release(account);
+    }
+
+    function _pendingPayment(
+        address account,
+        uint256 totalReceived,
+        uint256 alreadyReleased
+    ) private view override returns (uint256) {
+        return (totalReceived * _shares[account]) / totalShares - alreadyReleased;
+    }
 
     function getCount() public view returns (uint256) {
         return count;
--- test/counter.test.ts
+++ test/counter.test.ts
@@ -3,19 +3,31 @@ import { ethers } from 'hardhat'
 import '@nomiclabs/hardhat-ethers'
 
 import { Counter__factory, Counter } from '../build/types'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { BigNumber } from 'ethers'
 
 const { getContractFactory, getSigners } = ethers
 
 describe('Counter', () => {
   let counter: Counter
+  let signers: SignerWithAddress[]
+  let owner: SignerWithAddress
+  let others: SignerWithAddress[]
 
   beforeEach(async () => {
     // 1
-    const signers = await getSigners()
-
+    signers = await getSigners()
+    owner = signers[0]
+    others = signers.slice(1)
     // 2
     const counterFactory = (await getContractFactory('Counter', signers[0])) as Counter__factory
-    counter = await counterFactory.deploy()
+    const share = Math.floor(100 / signers.length)
+    counter = await counterFactory.deploy(
+      'SlickToken',
+      'SLK',
+      signers.map((s) => s.address),
+      Array.from({ length: signers.length }, () => share),
+    )
     await counter.deployed()
     const initialCount = await counter.getCount()
 
@@ -24,30 +36,62 @@ describe('Counter', () => {
     expect(counter.address).to.properAddress
   })
 
-  // 4
-  describe('count up', async () => {
-    it('should count up', async () => {
-      await counter.countUp()
-      const count = await counter.getCount()
-      expect(count).to.eq(1)
+  it('should allow authorized team members to deposit rewards', async () => {
+    const amount = ethers.utils.parseEther('1.0')
+    await signers[1].sendTransaction({
+      to: counter.address,
+      value: amount,
     })
+    const releasable = await counter['releasable(address)'](signers[1].address)
+    expect(releasable.gt(0)).to.eq(true)
   })
 
-  describe('count down', async () => {
-    // 5
-    it('should fail due to underflow exception', async () => {
-      const tx = counter.countDown()
-      await expect(tx).revertedWith(
-        'VM Exception while processing transaction: reverted with panic code 0x11 (Arithmetic operation underflowed or overflowed outside of an unchecked block)',
-      )
+  it('should reflect an accurate releasable amount given the number of team members and total deposits', async () => {
+    const amount = ethers.utils.parseEther('1.0')
+    const expected = amount.div(signers.length)
+    await owner.sendTransaction({
+      to: counter.address,
+      value: amount, // Sends exactly 1.0 ether
     })
+    const releasable = await counter['releasable(address)'](owner.address)
+    expect(releasable.eq(expected)).to.eq(true)
+  })
 
-    it('should count down', async () => {
-      await counter.countUp()
-
-      await counter.countDown()
-      const count = await counter.getCount()
-      expect(count).to.eq(0)
+  it('should ignore deposits from unauthorized team members', async () => {
+    const amount = ethers.utils.parseEther('1.0')
+    await owner.sendTransaction({
+      to: counter.address,
+      value: amount, // Sends exactly 1.0 ether
     })
+    const w = ethers.Wallet.createRandom()
+    const shares = await counter.shares(w.address)
+    expect(shares.eq(0)).to.eq(true)
+  })
+
+  it('should deposit rewards to the pool of users, not individual users', async () => {
+    const amount = ethers.utils.parseEther('1.0')
+    const otherGuyBalance = await signers[1].getBalance()
+    await owner.sendTransaction({
+      to: counter.address,
+      value: amount, // Sends exactly 1.0 ether
+    })
+    expect(otherGuyBalance).to.eq(otherGuyBalance)
+  })
+
+  it('should be able to withdraw their deposits', async () => {
+    const amount = ethers.utils.parseEther('1.0')
+    await owner.sendTransaction({
+      to: counter.address,
+      value: amount,
+    })
+    const postSendBalance = await owner.getBalance()
+    await counter['release(address)'](owner.address)
+    const newBalance = await owner.getBalance()
+    expect(newBalance.gt(postSendBalance)).to.eq(true)
+  })
+
+  it('should not get rewards for deposits distributed before deposits', async () => {
+    const tx = counter['release(address)'](owner.address)
+    await expect(tx).revertedWith('Counter: account has not deposited')
   })
 })
